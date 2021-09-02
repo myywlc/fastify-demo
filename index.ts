a7338de5ae977b5f981d0e3dff21fe61f1965f71
jiangchengbiao <jiangchengbiao@aidigger.com>
2021-09-02 18:43:36 +0800

    init

---
+++ index.ts
@@ -0,0 +1,38 @@
+import fastify from 'fastify';
+
+const server = fastify();
+
+interface IQuerystring {
+  username: string;
+  password: string;
+}
+
+interface IHeaders {
+  'h-Custom': string;
+}
+
+
+server.get('/ping', async (request, reply) => {
+  return 'pong\n';
+});
+
+server.get<{ Querystring: IQuerystring, Headers: IHeaders }>('/auth', {
+  preValidation: (request, reply, done) => {
+    const { username, password } = request.query;
+    done(username !== 'admin' ? new Error('Must be admin') : undefined);
+  },
+}, async (request, reply) => {
+  const { username, password } = request.query;
+  const customerHeader = request.headers['h-Custom'];
+  // 处理请求数据
+
+  return `logged in!`;
+});
+
+server.listen(3000, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
